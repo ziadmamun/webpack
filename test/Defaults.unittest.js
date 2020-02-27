63828f62afe2f78cbe04994c7bf81ce3d9adc707
evilebottnawi <sheo13666q@gmail.com>
2020-02-27 20:03:59 +0300

    feat: support `[query]` for asset modules

--- test/Defaults.unittest.js
+++ test/Defaults.unittest.js
@@ -189,7 +189,7 @@ describe("Defaults", () => {
 		    "usedExports": true,
 		  },
 		  "output": Object {
-		    "assetModuleFilename": "[hash][ext]",
+		    "assetModuleFilename": "[hash][ext][query]",
 		    "chunkCallbackName": "webpackChunkwebpack",
 		    "chunkFilename": "[name].js",
 		    "chunkLoadTimeout": 120000,
@@ -805,7 +805,7 @@ describe("Defaults", () => {
 			-           "idHint": "vendors",
 			-           "priority": -10,
 			-           "reuseExistingChunk": true,
-			-           "test": /[\\\\/]node_modules[\\\\/]/i,
+			-           "test": /[\\\\\\/]node_modules[\\\\\\/]/i,
 			-         },
 			-       },
 			-       "chunks": "async",
